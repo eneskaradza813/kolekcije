d3c800616966ec3ddc7e7a74593ab88e890e0906
Enes <Enes@Enes>
2020-01-07 22:55:57 +0100

    1 kolekcije = vjezba add.metoda

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,46 @@
+package javaapplication1;
+
+import java.util.ArrayList;
+
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+
+        
+        ArrayList usersToAdd = new ArrayList();
+        usersToAdd.add(new User(4, "Goran"));
+        usersToAdd.add(new User(2, "Ilija"));
+        usersToAdd.add(new User(6, "Nikola"));
+        
+        ArrayList users = new ArrayList();
+        users.add(new User(1, "Petar"));
+        users.add(new User(2, "Jovan"));
+        users.add(new User(3, "Zoran"));
+        
+        for (int i = 0; i < usersToAdd.size(); i++) {
+            boolean userExists = false;
+            for (int j = 0; j < users.size(); j++) {
+                if (((User) users.get(j)).id == ((User) usersToAdd.get(i)).id) {
+                    users.set(j, (User)usersToAdd.get(i));
+                    userExists = true;
+                }
+                if(!userExists)
+                    users.add(usersToAdd.get(i));
+                else
+                    userExists = false;
+            }
+            for(Object o : users){
+                User j = (User) o;
+                System.out.println(j.id + ":" + j.name);
+            }
+            
+        }
+        
+           
+        
+        
+        
+        
+    }
+
+}
---
+++ src/javaapplication1/User.java
@@ -0,0 +1,14 @@
+package javaapplication1;
+
+public class User {
+
+    
+    public int id;
+    public String name;
+    
+    public User(int id, String name){
+        this.id = id;
+        this.name = name;
+    
+    }
+}
